188afda9b35f2ee8cebc0e77f3d4d1addc28060b
Maxwell Pollack <maxwell_pollack@zoho.com>
2019-03-14 11:32:13 -0700

    Just a test commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,8 @@
 package com.company;
 
 public class Main {
-
+    boolean[][] board = new boolean[10][10];
     public static void main(String[] args) {
-        // write your code here
+
     }
 }
